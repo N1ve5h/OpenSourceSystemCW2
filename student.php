2509a667c61c419e8c7ce1da66746a09f3a68a2b
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2019-05-22 20:56:30 +0100

    Task 3

--- student.php
+++ student.php
@@ -13,21 +13,39 @@ if (isset($_SESSION['id'])) {
 
    // Build SQL statment that selects a student's modules
    $sql = "select * from student";
-
    $result = mysqli_query($conn,$sql);
 
    // prepare page content
    $data['content'] .= "<table border='1'>";
    $data['content'] .= "<tr><th colspan='5' align='center'>Student</th></tr>";
-   $data['content'] .= "<tr><th>Student ID</th><th>Password</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th></tr>";
+   $data['content'] .= "<tr><th></th><th>Student ID</th><th>Password</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th></tr>";
    // Display the modules within the html table
    while($row = mysqli_fetch_array($result)) {
-      $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[password] </td><td> $row[dob] </td><td> $row[firstname] </td><td> $row[lastname] </td><td> $row[house] </td><td> $row[town] </td>";
+      $data['content'] .= "<tr><td><form action='' method='post'><input type='checkbox' name='student[]'</td><td> $row[studentid] </td><td> $row[password] </td><td> $row[dob] </td><td> $row[firstname] </td><td> $row[lastname] </td><td> $row[house] </td><td> $row[town] </td>";
+      $studentdata = array //Store student information into an array
+      (
+        array("$row[studentid]","$row[password]","$row[dob]","$row[firstname]","$row[lastname]","$row[house]","$row[town]")
+      );
    }
+   echo "<input type='submit' name='delrecords' value='Delete'></form>";
    $data['content'] .= "</table>";
+
+   if (isset($_POST['delrecords']))
+   {
+     $i = 0;
+     while ($i < count($studentdata,0)) //Goes through all checkboxes have been ticked or not
+     {
+       if(isset($_POST['student["i"]'])) //delete if checked
+       {
+         mysqli_query($conn,"DELETE FROM student WHERE $studentdata[$i]");
+         echo "$studentdata[$i]";
+       }
+     }
+   }
 
    // render the template
    echo template("templates/default.php", $data);
+
 
    } else {
    header("Location: index.php");
